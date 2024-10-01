3bd56feec2f9a391d3492908da389c412d0127cb
Sulnklm <hyeinsuin@icloud.com>
2024-09-30 19:34:06 -0700

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg primary-color ">
-            <div className="container-fluid">
-                <a className="navbar-brand fw-bolder" href="#">Puffin</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                     <span class="navbar-toggler-icon"></span>
-                 </button>
+        <div>
+            <nav className="navbar navbar-expand-lg primary-bg-color">
+                <div className="container-fluid mx-lg-3">
+                    <a className="navbar-brand fw-bolder text-white" href="#">Puffin</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Menu</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-                        <li>
-                             <button type="button" className="btn btn-light">Order Now</button>   
-                        </li>
-                    </ul>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto flex-colum align-items-center">
+                            <li className="nav-item mb-4 mb-lg-0 mx-lg-2">
+                                <a className="nav-link text-white" aria-current="page" href="#">About</a>
+                            </li>
+                            <li className="nav-item mb-4 mb-lg-0 mx-lg-2">
+                                <a className="nav-link text-white" href="#">Menu</a>
+                            </li>
+                            <li className="nav-item mb-4 mb-lg-0 mx-lg-2">
+                                <a className="nav-link text-white" href="#">Contact</a>
+                            </li>
+                            <li className='mb-4 mb-lg-0 ms-lg-2'>
+                                <button type="button" className="btn btn-light primary-color rounded-pill">Order Now</button>   
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
